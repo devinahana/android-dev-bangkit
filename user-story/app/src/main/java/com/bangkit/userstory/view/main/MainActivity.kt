d48d3ad042407843fd8ba2095391597b2c6135b3
Devina <devina.hana@ui.ac.id>
2024-05-21 11:45:47 +0700

    chore: remove unused import

--- user-story/app/src/main/java/com/bangkit/userstory/view/main/MainActivity.kt
+++ user-story/app/src/main/java/com/bangkit/userstory/view/main/MainActivity.kt
@@ -7,7 +7,6 @@ import android.os.Bundle
 import android.provider.Settings
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
 import android.view.WindowInsets
 import android.view.WindowManager
 import androidx.activity.viewModels
